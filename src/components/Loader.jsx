02ee2d31195a28dd466401216c70c1d97614d322
Sebastian Pulido <sebastian.pulido@ibisdev.tech>
2023-03-21 09:31:46 -0300

    change to CircularProgress

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,19 +1,30 @@
 import { Html, useProgress } from "@react-three/drei";
 import { Box, Typography } from "@mui/material";
-import LinearProgress from "@mui/material/LinearProgress";
+import CircularProgress from "@mui/material/CircularProgress";
 
 export const Loader = () => {
   const { progress } = useProgress();
   return (
     <Html center>
-      <Box sx={{ display: "flex", alignItems: "center" }}>
-        <Box sx={{ width: 320 }}>
-          <LinearProgress variant="determinate" value={progress} />
-        </Box>
-        <Box sx={{ minWidth: 35 }}>
-          <Typography variant="body2" color="text.secondary">{`${Math.round(
-            progress
-          )}%`}</Typography>
+      <Box sx={{ position: "relative", display: "inline-flex" }}>
+        <CircularProgress size={80} variant="determinate" value={progress} />
+        <Box
+          sx={{
+            top: 0,
+            left: 0,
+            bottom: 0,
+            right: 0,
+            position: "absolute",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Typography
+            variant="body2"
+            component="div"
+            color="text.secondary"
+          >{`${Math.round(progress)}%`}</Typography>
         </Box>
       </Box>
     </Html>
